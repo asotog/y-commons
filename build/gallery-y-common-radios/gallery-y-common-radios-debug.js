b5b3b0a18480bb80f279f6c3b6f484188faf102e
asotog88 <asotog88@yahoo.es>
2013-11-01 10:42:43 -0600

    code docs added and also added Common namespace for classes

--- build/gallery-y-common-radios/gallery-y-common-radios-debug.js
+++ build/gallery-y-common-radios/gallery-y-common-radios-debug.js
@@ -1,72 +1,128 @@
 YUI.add('gallery-y-common-radios', function (Y, NAME) {
 
- Y.CommonRadios = Y.Base.create('gallery-y-common-radios', Y.Base, [], {
-     
-     initializer: function () {
-         var me = this;
-         Y.all(this.get('radioFieldSelector')).each(function (node, index) {
-             me.bindRadio(node);
-         });
-     },
+/**
+ * Utility to generate base markup to give a skin or different look and feel to radio buttons tags
+ *
+ * @class Radios
+ * @namespace Common
+ * @extends Base
+ * @module gallery-y-common-radios
+ * @constructor
+ */
 
-     bindRadio: function (node) {
-         var me = this;
-         /* init */
-         node.all(this.get('radioInputSelector')).each(function (node) {
-             var parent = node.get('parentNode');
-             if (!parent.hasClass(me.get('radioSelector'))) {
-                 parent.addClass(me.get('radioSelector'));
-                 var checkedClass = node.get('checked') ? ' checked' : '';
-                 parent.prepend(Y.Lang.sub(me.get('maskHtml'), {
-                     checked: checkedClass
-                 }));
-             }
-         });
-         
-         /* binds events */
-         node.delegate('click', function (e) {
-             node.all(me.get('maskSelector')).removeClass('checked');
-             var parent = e.currentTarget.get('parentNode');
-             parent.one(me.get('maskSelector')).addClass('checked');
-         }, this.get('radioInputSelector'));
-         
-         /* listen refresh */
-         this.on('refresh', function () {
-             node.all('input').each(function (radio) {
-                 var checked = radio.get('checked');
-                 var parent = radio.get('parentNode');
-                 if (!checked) {
-                     parent.one(me.get('maskSelector')).removeClass('checked');
-                 } else {
-                     parent.one(me.get('maskSelector')).addClass('checked');
-                 }
-             });
-         });
-     }
+Y.namespace('Common');
+
+/**
+ * Helper event that can be fired when object is instanced to refresh all the  radios associated to component instance
+ *
+ * @event refresh 
+ */
+var EVT_REFRESH = 'refresh';
+
+Y.Common.Radios = Y.Base.create('gallery-y-common-radios', Y.Base, [], {
+
+    initializer: function () {
+        var me = this;
+        Y.all(this.get('radioFieldSelector')).each(function (node, index) {
+            me.bindRadio(node);
+        });
+    },
+
+    bindRadio: function (node) {
+        var me = this;
+        /* init */
+        node.all(this.get('radioInputSelector')).each(function (node) {
+            var parent = node.get('parentNode');
+            if (!parent.hasClass(me.get('radioContainerClass'))) {
+                parent.addClass(me.get('radioContainerClass'));
+                var checkedClass = node.get('checked') ? ' checked' : '';
+                parent.prepend(Y.Lang.sub(me.get('maskHtml'), {
+                    checked: checkedClass
+                }));
+            }
+        });
+
+        /* binds events */
+        node.delegate('click', function (e) {
+            node.all(me.get('maskSelector')).removeClass('checked');
+            var parent = e.currentTarget.get('parentNode');
+            parent.one(me.get('maskSelector')).addClass('checked');
+        }, this.get('radioInputSelector'));
+
+        /* listen refresh */
+        this.on(EVT_REFRESH, function () {
+            node.all('input').each(function (radio) {
+                var checked = radio.get('checked');
+                var parent = radio.get('parentNode');
+                if (!checked) {
+                    parent.one(me.get('maskSelector')).removeClass('checked');
+                } else {
+                    parent.one(me.get('maskSelector')).addClass('checked');
+                }
+            });
+        });
+    }
 
 
- }, {
-     ATTRS: {
-         radioFieldSelector: {
-             value: '.custom-radio-buttons'
-         },
+}, {
+    ATTRS: {
+        /**
+         * Radio buttons group container selector
+         *
+         * @attribute radioFieldSelector
+         * @type String
+         * @default '.custom-radio-buttons'
+         */
+        radioFieldSelector: {
+            value: '.custom-radio-buttons'
+        },
 
-         maskSelector: {
-             value: '.radio-mask'
-         },
 
-         radioSelector: {
-             value: 'radio'
-         },
+        /**
+         * Mask node selector
+         *
+         * @attribute maskSelector
+         * @type String
+         * @default '.radio-mask'
+         */
+        maskSelector: {
+            value: '.radio-mask'
+        },
 
-         radioInputSelector: {
-             value: 'input[type="radio"]'
-         },
 
-         maskHtml: {
-             value: '<span class="radio-mask {checked}"></span>'
-         }
-     }
- });
+        /**
+         * Radio button wrapper class
+         *
+         * @attribute radioContainerClass
+         * @type String
+         * @default 'radio'
+         */
+        radioContainerClass: {
+            value: 'radio'
+        },
+
+        /**
+         * Radio tag field selector
+         *
+         * @attribute radioInputSelector
+         * @type String
+         * @default 'input[type="radio"]'
+         */
+        radioInputSelector: {
+            value: 'input[type="radio"]'
+        },
+
+        /**
+         * Mask html
+         *
+         * @attribute maskHtml
+         * @type String
+         * @default '<span class="radio-mask {checked}"></span'
+         */
+        maskHtml: {
+            value: '<span class="radio-mask {checked}"></span>'
+        }
+    }
+});
 
 }, '@VERSION@', {"requires": ["yui-base", "base-build"]});
--- build/gallery-y-common-selects/gallery-y-common-selects-debug.js
+++ build/gallery-y-common-selects/gallery-y-common-selects-debug.js
@@ -1,64 +1,117 @@
 YUI.add('gallery-y-common-selects', function (Y, NAME) {
 
-Y.CommonSelects = Y.Base.create('gallery-y-common-selects', Y.Base, [], {
-        initializer: function () {
-            this.syncUIComponents();
-        },
-        
-        syncUIComponents: function() {
-            var me = this;
-            Y.all(this.get('selectFieldSelector')).each(function (node, index) {
-                var parent = node.get('parentNode');
-                if (!parent.hasClass(me.get('containerClass'))) {
-                    parent.addClass(me.get('containerClass'));
-                    parent.prepend(me.get('maskHtml'));
-                    me.bindSelect(parent);
-                }
-            });
-        },
-        
-        bindSelect: function(node) {
-            var me = this;
-            var select = node.one('select');
-            var mask = node.one(this.get('maskSelector'));
+/**
+ * Utility to generate base markup to give a skin or different look and feel to <code>select</code> tags
+ *
+ * @class Selects
+ * @namespace Common
+ * @extends Base
+ * @module gallery-y-common-selects
+ * @constructor
+ */
+
+Y.namespace('Common');
+
+/**
+ * Helper event that can be fired when object is instanced to refresh all the  selects associated to component instance
+ *
+ * @event refresh 
+ */
+var EVT_REFRESH = 'refresh';
+
+Y.Common.Selects = Y.Base.create('gallery-y-common-selects', Y.Base, [], {
+    initializer: function () {
+        this.syncUIComponents();
+    },
+    
+    /**
+     * Iterates over all the selects and adds the respective markup and attaches events
+     *
+     * @method syncUIComponents
+     */
+    syncUIComponents: function () {
+        var me = this;
+        Y.all(this.get('selectFieldSelector')).each(function (node, index) {
+            var parent = node.get('parentNode');
+            if (!parent.hasClass(me.get('containerClass'))) {
+                parent.addClass(me.get('containerClass'));
+                parent.prepend(me.get('maskHtml'));
+                me.bindSelect(parent);
+            }
+        });
+    },
+
+    bindSelect: function (node) {
+        var me = this;
+        var select = node.one('select');
+        var mask = node.one(this.get('maskSelector'));
+        mask.set('text', me.getOptionText(select));
+        this.on(EVT_REFRESH, function () {
             mask.set('text', me.getOptionText(select));
-            this.on('refresh', function() {
-                mask.set('text', me.getOptionText(select));
-            });
-            select.on('change', function(e) {
-                var optionText = me.getOptionText(select);
-                mask.set('text', optionText);
-            });
+        });
+        select.on('change', function (e) {
+            var optionText = me.getOptionText(select);
+            mask.set('text', optionText);
+        });
+    },
+
+    getOptionText: function (node) {
+        var checkedOpt = node.one('option:checked');
+        var text = '';
+        if (checkedOpt) {
+            text = checkedOpt.get('text');
+        }
+        return text;
+    }
+
+
+}, {
+    ATTRS: {
+        /**
+         * Select tag field selector
+         *
+         * @attribute selectFieldSelector
+         * @type String
+         * @default 'select'
+         */
+        selectFieldSelector: {
+            value: 'select'
         },
-        
-        getOptionText: function(node) {
-            var checkedOpt = node.one('option:checked');
-            var text = '';
-            if (checkedOpt) {
-                text = checkedOpt.get('text');
-            }
-            return text;
+
+        /**
+         * Select tag container class
+         *
+         * @attribute containerClass
+         * @type String
+         * @default 'custom-select'
+         */
+        containerClass: {
+            value: 'custom-select'
+        },
+
+        /**
+         * Mask node selector
+         *
+         * @attribute maskSelector
+         * @type String
+         * @default '.custom-select-mask'
+         */
+        maskSelector: {
+            value: '.custom-select-mask'
+        },
+
+
+        /**
+         * Mask html
+         *
+         * @attribute maskHtml
+         * @type String
+         * @default '<span class="custom-select-mask"></span>'
+         */
+        maskHtml: {
+            value: '<span class="custom-select-mask"></span>'
         }
-        
-        
-    }, {
-        ATTRS: {
-            selectFieldSelector: {
-                value: 'select'
-            },
-            
-            containerClass: {
-                value: 'custom-select'
-            },
-            
-            maskSelector: {
-                value: '.custom-select-mask'
-            },
-            
-            maskHtml: {
-                value: '<span class="custom-select-mask"></span>'
-            }
-        }
-    });
+    }
+});
 
 }, '@VERSION@', {"requires": ["yui-base", "base-build"]});
