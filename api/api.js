ffb6baaf730347dfd9f0f34a1064168086919bd8
asotog88 <asotog88@yahoo.es>
2013-11-01 15:10:49 -0600

    initial documentation

---
+++ api/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Common.Radios",
+        "Common.Selects"
+    ],
+    "modules": [
+        "gallery-y-common-radios",
+        "gallery-y-common-selects"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-y-common-radios",
+            "name": "gallery-y-common-radios",
+            "description": "Utility to generate base markup to give a skin or different look and feel to radio buttons tags"
+        },
+        {
+            "displayName": "gallery-y-common-selects",
+            "name": "gallery-y-common-selects",
+            "description": "Utility to generate base markup to give a skin or different look and feel to <code>select</code> tags"
+        }
+    ]
+} };
+});
