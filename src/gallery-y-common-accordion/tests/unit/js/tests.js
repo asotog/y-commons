72eaf76fa0eb329892f70dada594695c4b7c75ef
asotog88 <asotog88@yahoo.es>
2014-02-05 13:38:04 -0600

    accordion tests

--- src/gallery-y-common-accordion/tests/unit/js/tests.js
+++ src/gallery-y-common-accordion/tests/unit/js/tests.js
@@ -4,12 +4,14 @@ YUI.add('module-tests', function(Y) {
 
     suite.add(new Y.Test.Case({
         name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.fail('No Tests Provided For This Module');
+        'accordion items gets expanded': function() {
+        	var acc = Y.one('#sample-accordion2');
+        	acc.one('.accordion-item .accordion-item-link').simulate('click');
+            Y.Assert.areEqual(true, acc.one('.accordion-item').hasClass('selected'), 'accordion expands item');
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 
-},'', { requires: [ 'test' ] });
+},'', { requires: [ 'test', 'node', 'node-event-simulate' ] });
