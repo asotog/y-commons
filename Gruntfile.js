9742c4fb31dfa7f40cfafdf4a9b50e8cbe9f66c3
asotog <asotog88@yahoo.es>
2014-05-27 15:27:38 -0600

    initial ractive module commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,20 @@ module.exports = function(grunt) {
 
     var cli = grunt.cli;
 
+    grunt.initConfig({
+	  uglify: {
+	    ractive: {
+	      files: {
+	        'src/gallery-y-common-ractive/js/ractive.min.js': ['src/gallery-y-common-ractive/ractive-tmp/ractive.js']
+	      }
+	    }
+	  }
+	});
    
     grunt.loadNpmTasks('grunt-yui-contrib');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-css-selectors');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['boot']);
 
