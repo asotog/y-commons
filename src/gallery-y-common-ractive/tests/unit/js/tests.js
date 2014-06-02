1c45e4646f4806fbe4b78107c3ba1c635a84cb89
asotog <asotog88@yahoo.es>
2014-06-02 15:48:10 -0600

    style decorator added

--- src/gallery-y-common-ractive/tests/unit/js/tests.js
+++ src/gallery-y-common-ractive/tests/unit/js/tests.js
@@ -9,7 +9,15 @@ YUI.add('module-tests', function (Y) {
             this.wait(function () {
                 Y.Assert.isNull(Y.one('#el img'));
             }, 1000);
+         },
+        
+        'style decorator test': function () {
+            this.wait(function () {
+                Y.Assert.areEqual(Y.one('.style-test').getAttribute('style'), 'width: 400px; height: 200px;background: url(http://placehold.it/48x48)');
+            }, 1000);
          }
+        
+        
     }));
 
     Y.Test.Runner.add(suite);
